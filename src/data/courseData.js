28503483fd2c7bd8f72dd919c8d70c0f21b2f395
张红豆 <zhanghongdou@enn.cn>
2020-11-03 01:10:14 +0800

    搜索demo

---
+++ src/data/courseData.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1749261970,641813381&fm=26&gp=0.jpg',
+    courseName: 'Web前端开发'
+  },
+  {
+    img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1604345001995&di=8f8dd58e5421b2d3ffdf5edb5d1dac90&imgtype=0&src=http%3A%2F%2F2a.zol-img.com.cn%2Fproduct%2F92_501x2000%2F122%2FcepO8BMCbxf2.gif',
+    courseName: 'Java开发'
+  },
+  {
+    img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2081902785,4183342680&fm=26&gp=0.jpg',
+    courseName: 'Python开发'
+  }
+]
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="about">
+  <input type="text" placeholder="请搜索" @input="searchAction($event)">
+  {{searchResult}}
+  <ul v-if="searchResult.length > 0">
+    <li v-for="(item, index) in searchResult" :key="index">
+      <div>
+        <img :src="item.img" :alt="item.courseName">
+        <h1>{{ item.courseName }}</h1>
+      </div>
+    </li>
+  </ul>
+  <ul v-else>
+    <li v-for="(item, index) in courseData" :key="index">
+      <div>
+        <img :src="item.img" :alt="item.courseName">
+        <h1>{{ item.courseName }}</h1>
+      </div>
+    </li>
+  </ul>
+</div>
 </template>
+
+<script>
+import courseData from '@/data/courseData'
+import {
+  reactive,
+  toRefs,
+} from 'vue';
+
+export default {
+  name: 'search',
+  setup() {
+    const state = reactive({
+      searchResult: [],
+    })
+
+    const searchAction = (e) => {
+      const inputVal = e.target.value;
+      state.searchResult = courseData.filter(item => item.courseName.includes(inputVal))
+      console.log('searchResult', state.searchResult)
+    }
+    return {
+      ...toRefs(state),
+      courseData,
+
+      searchAction,
+    }
+  },
+}
+</script>
